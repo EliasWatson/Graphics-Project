9ed4a41d096066201960a37abb00254b775180f7
Elias Watson <me@eliaswatson.dev>
2021-01-24 17:40:54 -0500

    Drawing point

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
 void init(GLFWwindow* window);
 void display(GLFWwindow* window, double current_time);
+GLuint createShaderProgram();
 
 int main() {
     // Initialize GLFW
@@ -35,10 +41,47 @@ int main() {
 }
 
 void init(GLFWwindow* window) {
-
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
 }
 
 void display(GLFWwindow* window, double current_time) {
-    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(renderingProgram);
+    glDrawArrays(GL_POINTS, 0, 1);
+}
+
+GLuint createShaderProgram() {
+    const char* vshaderSource = "           \
+        #version 430                        \
+        void main() {                       \
+            gl_Position = vec4(0, 0, 0, 1); \
+        }                                   \
+    ";
+
+    const char* fshaderSource = "           \
+        #version 430                        \
+        out vec4 color;                     \
+        void main() {                       \
+            color = vec4(0, 0, 1, 1);       \
+        }                                   \
+    ";
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
 }
