0a780d2905f4247aafcaf7ed0bc0234e64da2ca6
Elias Watson <me@eliaswatson.dev>
2021-03-09 05:47:12 -0500

    Cleaned up texture code

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,6 +1,22 @@
 #include "texture.hpp"
 
+#include "material.hpp"
+
 texture::texture(GLuint id, type tex_type) {
     this->id = id;
     this->tex_type = tex_type;
+}
+
+void texture::loadTexture(material* mat) {
+    glActiveTexture(GL_TEXTURE0 + this->tex_type);
+    glBindTexture(texture_gl_types[this->tex_type], this->id);
+    std::string name = texture_type_names[this->tex_type];
+    mat->shaderProgram.setFloat((name + "_sampler").c_str(), float(this->tex_type));
+    mat->shaderProgram.setFloat((name + "_sampler_contrib").c_str(), 1.0f);
+}
+
+void texture::clearTextureContrib(material* mat) {
+    for(int i = 0; i < NUM_TYPES; ++i) {
+        mat->shaderProgram.setFloat((texture_type_names[i] + "_sampler_contrib").c_str(), 0.0f);
+    }
 }
--- src/texture.hpp
+++ src/texture.hpp
@@ -4,17 +4,26 @@
 #include <GL/glew.h>
 #include <string>
 
+struct material;
+
 const std::string texture_type_names[3] = {
     "albedo",
     "roughness",
     "normal"
 };
 
+const GLenum texture_gl_types[3] = {
+    GL_TEXTURE_2D,
+    GL_TEXTURE_2D,
+    GL_TEXTURE_2D
+};
+
 struct texture {
     enum type {
-        ALBEDO,
-        ROUGHNESS,
-        NORMAL
+        ALBEDO      = 0,
+        ROUGHNESS   = 1,
+        NORMAL      = 2,
+        NUM_TYPES
     };
 
     GLuint id;
@@ -22,6 +31,9 @@ struct texture {
 
     texture() { }
     texture(GLuint id, type tex_type);
+
+    void loadTexture(material* mat);
+    static void clearTextureContrib(material* mat);
 };
 
 #endif
